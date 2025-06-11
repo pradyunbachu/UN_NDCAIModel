9237d5cfaecfd8b3707ecea805adecf3eaddd8e9
Pradyun Bachu <pradyun.bachu@gmail.com>
2025-06-10 18:01:09 -0700

    deleted unsued files

--- config.py
+++
@@ -1,7 +0,0 @@
-# Define API endpoints
-API_ENDPOINTS = {
-    'data_status': '/api/data_status',
-    'collect_data': '/api/collect_data',
-    'process_data': '/api/process_data',
-    'predict': '/api/predict'
-} 
